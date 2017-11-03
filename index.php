07ccff8c409ab93d3239651e1708e8c9b1556e8d
Cassidy Hales <cassidyhales11@gmail.com>
2017-11-03 16:17:22 -0600

    Learned about assoociative arrays

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
-$names = [
-  'Cassidy',
-  'Elle',
-  'Nichole'
+$person = [
+  'age' => 20,
+  'hair' => 'brown',
+  'position' => 'web developer'
 ];
+
+$person['name'] = 'Cassidy';
+
+unset($person['age']);
 
 
 require 'index.view.php';
--- index.view.php
+++ index.view.php
@@ -6,26 +6,15 @@
 
   <meta charset="UTF-8">
   <title>Document</title>
-  <style>
-    header {
-      background: #e3e3e3;
-      padding: 2em;
-      text-align: center;
-    }
-  </style>
 </head>
 
 <body>
-  <ul>
-    <?php foreach($names as $name) : ?>
-      <li><?= $name?></li>
-    <?php endforeach; ?>
 
-    <?php
-    foreach ($names as $name) {
-      echo "<li>$name</li>";
-      }
-    ?>
+  <ul>
+    <?php foreach ($person as $key => $feature) : ?>
+      <li><strong><?= $key; ?></strong> <?= $feature; ?></li>
+    <?php endforeach; ?>
   </ul>
+
 </body>
 </html>
